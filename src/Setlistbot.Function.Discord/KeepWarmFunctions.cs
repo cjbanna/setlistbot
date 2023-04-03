ee3bb2b323e9a5e82ffedacd22f8b8cfc127d8f4
Chase Banna <cjbanna@gmail.com>
2023-04-02 18:34:29 -0700

    Add keep warm timer function (#11)

---
+++ src/Setlistbot.Function.Discord/KeepWarmFunctions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Setlistbot.Function.Discord
+{
+    public class KeepWarmFunctions
+    {
+        private readonly ILogger<KeepWarmFunctions> _logger;
+
+        public KeepWarmFunctions(ILogger<KeepWarmFunctions> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("KeepWarm")]
+        [SuppressMessage(
+            "Style",
+            "IDE0060:Remove unused parameter",
+            Justification = "The Azure functions library requires the TimerInfo parameter"
+        )]
+        public void KeepWarm([TimerTrigger("* */3 * * * *")] object timerInfo)
+        {
+            _logger.LogInformation("Keep warm function called");
+        }
+    }
+}
