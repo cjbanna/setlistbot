8e6779e02cc12a8ed856a2c03fc31cda8313bee5
Chase Banna <cjbanna@gmail.com>
2024-01-16 16:38:23 -0800

    Add unit test

---
+++ test/Setlistbot.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace Setlistbot.Domain.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void NewSong()
+        {
+            var song = new Song("foo", 1, ">", 100, "footnote");
+            Assert.NotNull(song);
+        }
+
+        [Fact]
+        public void WithFootnote()
+        {
+            var song = new Song("foo", 1, ">", 100, "");
+            var newSong = song.WithFootnote("footnote");
+            Assert.Equal("footnote", newSong.Footnote);
+        }
+    }
+}
